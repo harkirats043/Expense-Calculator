0dac1a96784a4f4c2b8d3075a1c6df92bf79b2c8
Harkirats043 <Harkirats043@gmail.com>
2023-10-18 12:10:40 -0400

    added date

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -4,7 +4,7 @@ import ExpenseDate from "./ExpenseDate"
 export default function ExpenseItem(props) {
     return (
         <div className="expense-item">
-           <ExpenseDate />
+           <ExpenseDate expense={props.expense}/>
             <div className="expense-item__description">
                 <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
